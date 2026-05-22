5fd84a9d19b4b524b32c2605e6b8df10f8adc02a
Felix <cmekusfelix8@gmail.com>
2026-05-22 21:43:36 +0100

    Adding a simple DieGuess game

---
+++ DiceGame/Program.cs
@@ -0,0 +1,81 @@
+﻿GuessingGame game = new GuessingGame(new Die(new Random()), new ConsoleInterface());
+game.Run();
+
+public class Die
+{
+    private readonly Random _random;
+
+    public Die(Random random)
+    {
+        _random = random;
+    }
+
+    public int Roll()
+    {
+        return _random.Next(1, 7);
+    }
+}
+
+public class ConsoleInterface
+{
+    public void PrintMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public string ReadInput()
+    {
+        return Console.ReadLine();
+    }
+}
+
+public class GuessingGame
+{
+    private readonly int _targetNumber;
+    private readonly ConsoleInterface _console;
+    private int _remainingChances = 3;
+
+    public GuessingGame(Die die, ConsoleInterface console)
+    {
+        _targetNumber = die.Roll();
+        _console = console;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            _console.PrintMessage("Dice rolled. Enter what number it shows in three tries.");
+            string? input = _console.ReadInput();
+
+            if (!int.TryParse(input, out int guess))
+            {
+                _console.PrintMessage("Incorrect input, try again.");
+                continue;
+            }
+
+            if (guess == _targetNumber)
+            {
+                _console.PrintMessage("Congratulations, you won!");
+                WaitForExit();
+                return;
+            }
+
+            _remainingChances--;
+            _console.PrintMessage("Wrong number!");
+
+            if (_remainingChances == 0)
+            {
+                _console.PrintMessage("You lose");
+                WaitForExit();
+                return;
+            }
+        }
+    }
+
+    private static void WaitForExit()
+    {
+        Console.ReadKey(true);
+    }
+}
+
---
+++ OOP/Program.cs
@@ -0,0 +1,77 @@
+﻿MedicalAppointment medicalAppointment = new MedicalAppointment(
+    "Felix Ewulu", new DateTime(2026, 01, 01));
+
+medicalAppointment.Reschedule(new DateTime(2026, 05, 01));
+Console.ReadKey(true);
+
+
+class Person
+{
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+    public Person(string name, int yearOfBirth)
+    {
+        Name = name;
+        YearOfBirth = yearOfBirth;
+    }
+}
+
+class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+    public MedicalAppointment(string  patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    // When this constructor is called, it first executes the code from th constructor
+    // with two same-typed parameter, before executing this.
+    public MedicalAppointment(string patientName) :
+        this(patientName, 7)
+    {
+    }
+    
+    public MedicalAppointment(string  patientName, int daysFromNow)
+    {
+        _patientName = patientName;
+        _date = DateTime.Now.AddDays(daysFromNow);
+    }
+    
+    public DateTime GetDate() => _date;
+
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+        var printer = new MedicalAppointmentPrinter();
+        printer.Print(this);
+    }
+
+    public void Reschedule(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+}
+
+class MedicalAppointmentPrinter
+{
+    public void Print(MedicalAppointment medicalAppointment)
+    {
+        Console.WriteLine($"Appointment will take place on {medicalAppointment.GetDate()}");
+    }
+}
+
+class Rectangle
+{
+    // A readonly field can only be assigned at the declaration or in the constructor
+    public readonly int Width;
+    public readonly int Height;
+    
+    // Immutability means that once an object is created, it will never be modified.
+    // make fields readonly if possible.
+    // const modifier can be applied to both variables and fields.
+    // const fields must be assigned a value at declaration
+    // const fields are compile-time constant.
+    
+}
