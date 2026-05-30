54e385870e741c23770259875543aa858181ff7f
Felix <cmekusfelix8@gmail.com>
2026-05-30 15:49:52 +0100

    Finished section

---
+++ OOPADVANCED/Flyables/Bird.cs
@@ -0,0 +1,35 @@
+﻿namespace OOPADVANCED.Flyables;
+
+public interface IFlyable
+{
+    void Fly();
+}
+
+public interface IFuelable
+{
+    void Fuel();
+}
+
+public class Bird : IFlyable
+{
+    public void Tweet() =>
+    Console.WriteLine("Tweet");
+    
+    public void Fly() =>
+    Console.WriteLine("Flying using its wings.");
+}
+
+public class Kite : IFlyable
+{
+    public void Fly() =>
+    Console.WriteLine("Flying carried by the wind.");
+}
+
+public class Plane : IFlyable, IFuelable
+{
+    public void Fly() =>
+    Console.WriteLine("Flying using jet propulsion.");
+    
+    public void Fuel() =>
+    Console.WriteLine("Filling tanks with jet fuel.");
+}
